556724694f5aa61552116804462282be249613cf
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-17 16:40:14 -0600

    refactor create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to "/users/#{@user.id}"
+      redirect_to user_path(@user)
     else
+      redirect_to register_path
       flash[:notice] = @user.errors.full_messages.to_sentence
-      redirect_to '/register'
     end
   end
 
@@ -19,6 +19,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:name, :email)
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
