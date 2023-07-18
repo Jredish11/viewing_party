ddfbf3ce1f453a64e567c7b6ca9206c223bfd82e
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-17 20:34:19 -0600

    add tests for incorrect credentials for login form

--- spec/features/users/user_can_log_in_spec.rb
+++ spec/features/users/user_can_log_in_spec.rb
@@ -6,11 +6,25 @@ RSpec.describe "Logging In" do
     
     visit login_form_path
 
-    fill_in :email, with: "user@example.com"
+    fill_in :email, with: user.email
     fill_in :password, with: "password"
-
+    
     click_on "Log In"
 
     expect(current_path).to eq(root_path)
+  end
+
+  it "cannot log in with bad credentials" do
+    user = User.create(name: "name", email: "user@example.com", password: "password")
+    
+    visit login_form_path
+
+    fill_in :email, with: user.email
+    fill_in :password, with: "incorrect password"
+
+    click_on "Log In"
+
+    expect(current_path).to eq(login_form_path)
+    expect(page).to have_content("Sorry invalid credentials")
   end
 end
