92c6f2e615990e28489d07b55a15bf2ac8e3be82
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-18 16:30:16 -0600

    clean up actions to move into sessions controller for refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,34 +2,36 @@ class UsersController < ApplicationController
   def new
   end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to user_path(@user)
-    else
-      redirect_to register_path
-      flash[:notice] = @user.errors.full_messages.to_sentence
-    end
-  end
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     session[:user_id] = @user.id
+  #     redirect_to user_path(@user)
+  #   else
+  #     redirect_to register_path
+  #     flash[:notice] = @user.errors.full_messages.to_sentence
+  #   end
+  # end
 
   def show
     @user = User.find(params[:id])
   end
 
-  def login_form
+  # def login_form
 
-  end
+  # end
 
-  def login_user
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      flash[:success] = "Welcome, #{user.email}!"
-      redirect_to root_path
-    else
-      redirect_to login_form_path
-      flash[:error] = "Sorry invalid credentials"
-    end
-  end
+  # def login_user
+  #   user = User.find_by(email: params[:email])
+  #   if user && user.authenticate(params[:password])
+  #     session[:user_id] = user.id
+  #     flash[:success] = "Welcome, #{user.email}!"
+  #     redirect_to root_path
+  #   else
+  #     redirect_to login_form_path
+  #     flash[:error] = "Sorry invalid credentials"
+  #   end
+  # end
 
   private
 
