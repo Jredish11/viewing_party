1c40838d16de11389be09c2dac22663dbdc1895b
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-23 14:55:58 -0600

    delete md

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,36 +2,9 @@ class UsersController < ApplicationController
   def new
   end
 
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     session[:user_id] = @user.id
-  #     redirect_to user_path(@user)
-  #   else
-  #     redirect_to register_path
-  #     flash[:notice] = @user.errors.full_messages.to_sentence
-  #   end
-  # end
-
   def show
     @user = User.find(params[:id])
   end
-
-  # def login_form
-
-  # end
-
-  # def login_user
-  #   user = User.find_by(email: params[:email])
-  #   if user && user.authenticate(params[:password])
-  #     session[:user_id] = user.id
-  #     flash[:success] = "Welcome, #{user.email}!"
-  #     redirect_to root_path
-  #   else
-  #     redirect_to login_form_path
-  #     flash[:error] = "Sorry invalid credentials"
-  #   end
-  # end
 
   private
 
