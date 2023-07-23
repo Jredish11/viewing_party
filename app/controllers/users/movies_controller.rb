d6b0a8606ad12552a4c0f91b9dabd9776e13c01c
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-07-23 14:55:04 -0600

    update show action for visitor conditional

--- app/controllers/users/movies_controller.rb
+++ app/controllers/users/movies_controller.rb
@@ -7,5 +7,8 @@ class Users::MoviesController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @movie = MovieFacade.new(params).search
+    if !current_user
+      flash[:error] = "You must be logged in or registered user to create a viewing party"
+    end
   end
 end
